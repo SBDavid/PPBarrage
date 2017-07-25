9b81eb88e7b746d37b621a1d29fe89a18702d539
davidtangjw <davidtangjw@pptv.com>
2017-07-25 18:27:47 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ function barrage(ele) {
 var defalutConfig = {
     bulletHeight: 50,
     fireInterval: 200,
-    v: 50,
-    isDebug: true
+    displayTime: 6000,  // 弹幕显示的时长
+    isDebug: false,
+    fontSize: 24        // 弹幕字体大小
 }
 
 function log(isDebug, ...para) {
@@ -20,7 +21,7 @@ barrage.prototype.initTrack = function() {
     for (var i = 0; i < trackAmount; i++) {
         this.tracks.push({
             id: i,
-            top: i * this.config.bulletHeight
+            top: (i + 1) * this.config.bulletHeight
         });
 
         this.lastFired[i] = null;
@@ -47,9 +48,13 @@ function mountBullet(ele, bullet) {
 
 barrage.prototype.animate = function (time) {
     if (this.status === 'running') {
+        
         requestAnimationFrame(this.animate.bind(this));
+        this.tweenGroup.update(time);
+        this.print();
+        
+        this.fire();
     }
-    this.tweenGroup.update(time);
 }
 
 barrage.prototype.init = function (config) {
@@ -59,12 +64,16 @@ barrage.prototype.init = function (config) {
 
     this.bulletId = 0;
     this.bulletPool = [];
+    this.bulletRunningPool = new Map();
     this.tracks = [];
-    this.lastFired = {};
+    this.lastFired = {};                        // 一条弹幕中的最后一个，如果为空表示弹道是空的
     this.fireTimer = null;
     this.tweenGroup = new TWEEN.Group();
-    this.status = null;
-    this.nextBullet = null;
+    this.status = null;                         // 弹幕状态：init | running | idle
+    this.nextBullet = null;                     // 下一次要发射的弹幕
+    this.ctx = this.root.getContext('2d');
+    this.ctx.font = this.config.fontSize + 'px serif';
+    this.ctx.textBaseline = 'bottom';
 
     this.initTrack(this.config, this.tracks, this.root.offsetHeight);
 }
@@ -78,21 +87,71 @@ barrage.prototype.load = function (bullets) {
     log(this.config.isDebug, `装载子弹，子弹数量：${this.bulletPool.length}`);
     if (this.status && this.status === 'idle') {
         this.status = 'running';
-        this.fireTimer = setInterval(function () {
+        /* this.fireTimer = setInterval(function () {
             this.fire();
-        }.bind(this), this.config.fireInterval);
-        this.animate();
+        }.bind(this), this.config.fireInterval); */
+        requestAnimationFrame(this.animate.bind(this));
     }
 }
 
-barrage.prototype.fireBullet = function() {
-
+barrage.prototype.moveBullet = function(bullet) {
+    var step = new TWEEN.Tween(bullet.steps, this.tweenGroup)
+        .to({ left: this.root.offsetWidth + bullet.width }, this.config.displayTime)
+        .easing(TWEEN.Easing.Linear.None)
+        .onComplete(function () {
+            this.bulletRunningPool.delete(bullet.id);
+            if (this.bulletRunningPool.size === 0) {
+                this.status = 'idle';
+            }
+        }.bind(this));
+    step.start();
 }
 
-barrage.prototype.createBullet = function(msg) {
-    var bullet = {
-        msg: msg,
-        v: this.root.offsetHeight
+barrage.prototype.print = function() {
+    this.ctx.clearRect(0, 0, this.root.offsetWidth, this.root.offsetHeight);
+    var width = this.root.offsetWidth;
+    var height = this.root.offsetHeight;
+
+    this.bulletRunningPool.forEach(function(bullet){
+        // 半圆
+        this.ctx.fillStyle = "rgba(0,0,0,0.5)";
+        this.ctx.beginPath();
+        this.ctx.arc(
+            width - bullet.steps.left + this.config.fontSize / 2, 
+            this.tracks[bullet.trackId].top - this.config.fontSize / 2, 
+            this.config.fontSize / 2, 
+            -Math.PI / 2, Math.PI / 2, true);
+        this.ctx.fill();
+        // 矩形
+        this.ctx.fillRect(
+            width - bullet.steps.left + this.config.fontSize / 2,
+            this.tracks[bullet.trackId].top - this.config.fontSize,
+            bullet.width - this.config.fontSize,
+            this.config.fontSize);
+        // 半圆
+        this.ctx.beginPath();
+        this.ctx.arc(
+            width - bullet.steps.left - this.config.fontSize / 2 + bullet.width, 
+            this.tracks[bullet.trackId].top - this.config.fontSize / 2, 
+            this.config.fontSize / 2, 
+            Math.PI / 2, Math.PI * 1.5, true);
+        this.ctx.fill();
+        // 文字
+        this.ctx.fillStyle = "rgba(255,255,255,1)";
+        this.ctx.fillText(bullet.msg, 
+            width - bullet.steps.left + this.config.fontSize / 2, 
+            this.tracks[bullet.trackId].top);
+    },this);
+}
+
+barrage.prototype.createBullet = function(bullet) {
+    return {
+        id: bullet.id,
+        msg: bullet.msg,
+        width: utils.getTextBulletLength(this.ctx, bullet.msg, this.config.fontSize),
+        v: ( utils.getTextBulletLength(this.ctx, bullet.msg, this.config.fontSize) + this.root.offsetWidth ) / this.config.displayTime,
+        trackId: null,
+        steps: {left: 0}
     }
 }
 
@@ -100,28 +159,58 @@ barrage.prototype.getIdleTrack = function() {
     for(var i = 0; i < this.lastFired.length; i++) {
         if (this.lastFired[i] === null) {
             return i;
+        } else {
+            if (this.lastFired[i].steps.left < this.lastFired[i].width) {
+                continue;
+            } else if (this.lastFired[i].v >= this.nextBullet.v) {
+                return i;
+            } else if (this.lastFired[i].steps.left - this.lastFired[i].width >= (this.nextBullet.v - this.lastFired[i].v) * this.config.displayTime) {
+                return i;
+            }
         }
-
-
     }
+    return null;
 }
 
 barrage.prototype.fire = function() {
     if (this.bulletPool.length === 0) {
         log(this.config.isDebug, 'bulletPool已空，进入空闲状态');
-        clearInterval(this.fireTimer);
+        /* clearInterval(this.fireTimer); */
+        return;
     }
-    // 批量发射弹幕
+    if (this.nextBullet === null) {
+        var next = this.bulletPool.shift();
+        this.nextBullet = this.createBullet(next);
+    }
+
+    var trackId = this.getIdleTrack();
+    if (trackId === null) {
+        log(this.config.isDebug, '没有找到可用的弹道');
+        return;
+    }
+
+    this.nextBullet.trackId = trackId;
+    this.lastFired[trackId] =  this.nextBullet;
+    this.bulletRunningPool.set(this.nextBullet.id, this.nextBullet);
+
+    this.moveBullet(this.nextBullet);
+    this.nextBullet = null;
 }
 
 barrage.prototype.start = function() {
-    if (this.status || this.status !== 'init') {
+    if (this.status || this.status === 'init') {
         return;
     }
 
     this.status = 'running';
-    this.fireTimer = setInterval(function(){
+    /* this.fireTimer = setInterval(function(){
         this.fire();
-    }, this.config.fireInterval);
+    }.bind(this), this.config.fireInterval); */
+    requestAnimationFrame(this.animate.bind(this));
 }
 
+var utils = {
+    getTextBulletLength: function(ctx,msg,fontSize) {
+        return ctx.measureText(msg).width + fontSize;
+    },
+}
